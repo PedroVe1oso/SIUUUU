fff793e8b6d03a4b46023bf215025d713f162549
PedroVe1oso <93397845+PedroVe1oso@users.noreply.github.com>
2022-05-15 12:01:33 +0100

    Added connection to the database

---
+++ website/includes/config.php
@@ -0,0 +1,16 @@
+<?php
+    ob_start(); // turn on output buffering
+    session_start();
+
+    date_default_timezone_set("Europe/Lisbon"); // used to insert date and time to the database
+
+    try
+    {
+        $con = new PDO('sqlite:database.db');
+        $con -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        $con -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (PDOException $exception)
+    {
+        exit("Connection to database failed: " . $exception -> getMessage());
+    }
