486e83de823310c1ef1af3ba94a6303cccf02ef4
Pedro Veloso <up201906293@up.pt>
2022-06-14 14:37:18 +0100

    Added Register modal to login form

--- website/templates/common.tpl.php
+++ website/templates/common.tpl.php
@@ -15,6 +15,7 @@ require_once(__DIR__ . '/../includes/classes/Session.php');
         <link rel="stylesheet" type="text/css" href="/website/css/style.css">
         <link rel="stylesheet" type="text/css" href="/website/css/navbar.css">
         <link rel="stylesheet" type="text/css" href="/website/css/signUpIn.css">
+        <script src="../scripts/script.js" defer></script>
         <script src="https://kit.fontawesome.com/3b04e89a84.js" crossorigin="anonymous"></script>
     </head>
     <body>
@@ -87,7 +88,55 @@ require_once(__DIR__ . '/../includes/classes/Session.php');
             </form>
             <div class="divide"></div>
             <div class="registerButton">
-                <button onclick="document.location='register.php'">Create new account</button>
+<!--                <button onclick="document.location='/website/pages/register.php'">Create new account</button>-->
+                <button id="openRegisterModal">Create new account</button>
+            </div>
+            <div class="modalContainer" id="modalContainer">
+                <div class="signUpContainer">
+                    <div class="header">
+                        <h1>Sign Up</h1>
+                        <h2>It’s quick and easy.</h2>
+                    </div>
+                    <form method="POST">
+                        <div class="userDetails">
+                            <div class="form-input-group">
+                                <input type="text" name="firstName" placeholder="First name" required>
+                                <input type="text" name="lastName" placeholder="Last name" required>
+                            </div>
+                            <div class="form-input-group">
+                                <input type="email" name="email" placeholder="Email" required>
+                                <input type="tel" name="phoneNumber" placeholder="Phone number" pattern="[0-9]{9}" required>
+                            </div>
+                            <div class="form-input-group">
+                                <input type="password" name="password" placeholder="Password" required>
+                                <input type="password" name="password2" placeholder="Confirm password" required>
+                            </div>
+                            <div class="form-input-group">
+                                <label for="birthday">Birthday: <input type="date" id="birthday" name="birthday"></label>
+                            </div>
+                            <div class="form-input-group">
+                                <div>Gender:</div>
+                                <div class="genders">
+                                    <div>
+                                        <label for="genderMale">Male</label>
+                                        <input type="radio" id="genderMale" name="gender" value="Male">
+                                    </div>
+                                    <div>
+                                        <label for="genderFemale">Female</label>
+                                        <input type="radio" id="genderFemale" name="gender" value="Female">
+                                    </div>
+                                    <div>
+                                        <label for="genderIdk">Idk</label>
+                                        <input type="radio" id="genderIdk" name="gender" value="Idk">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="registerButton">
+                                <input id="closeRegisterModal1" type="submit" name="submitButton" value="Register">
+                            </div>
+                    </form>
+                        <a id="closeRegisterModal2" class="linkToLogIn">Already have an account? Log in here.</a>
+                </div>
             </div>
         </div>
     </section>
