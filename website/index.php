ffeca5cae5f94116d34c0d9dce9ebfff5f074fc6
Pedro Veloso <up201906293@up.pt>
2022-06-14 02:01:08 +0100

    Category template added and in use

--- website/index.php
+++ website/index.php
@@ -5,11 +5,17 @@ require_once(__DIR__ . '/includes/classes/Session.php');
 $session = new Session();
 
 require_once(__DIR__ . '/database/config.php');
+require_once(__DIR__ . '/database/classes/Category.php');
+
+
 require_once(__DIR__ . '/templates/common.tpl.php');
+require_once(__DIR__ . '/templates/category.tpl.php');
 
 $con = getDatabaseConnection();
 
-drawHeader($session);
+$categories = Category::getCategories($con, 10);
 
+drawHeader($session);
+drawCategories($categories);
 drawFooter();
 ?>
